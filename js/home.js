e2de6bbffa6a3685266f7ded016daa8c7a38d1c7
Alden Timme <alden.timme@gmail.com>
2012-10-16 16:05:11 -0700

    need to add _your_ email address as well!

--- js/home.js
+++ js/home.js
@@ -5,20 +5,18 @@ $('button.ask').click(function(e) {
   //$('.email2').text($('[name=email2]').val());
   //$('.email3').text($('[name=email3]').val());
 });
-$('button.add-reviewer').click(function(e) {
-  $()
-});
+//$('button.add-reviewer').click(function(e) {
+//  $()
+//});
 $('#reviewCarousel').carousel({
   interval: false
 });
 $('.carousel-next').click(function(e) {
   $('#reviewCarousel').carousel('next');
-  $('#reviewCarousel').carousel('pause');
   e.preventDefault();
 });
 $('.carousel-prev').click(function(e) {
   $('#reviewCarousel').carousel('prev');
-  $('#reviewCarousel').carousel('pause');
   e.preventDefault();
 });
 //$('#reviewCarousel').on('slid', function(a, b ,c) {
